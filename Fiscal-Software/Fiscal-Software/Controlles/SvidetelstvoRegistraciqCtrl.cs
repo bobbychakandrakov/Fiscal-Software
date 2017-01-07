cfea3e9601b335e52056581e2a437c7f4b823f3b
dinchokostadinov <dincho1998@abv.bg>
2017-01-07 13:18:07 +0200

    CRUD SvidetelstvoRegistraciq-Done!

---
+++ Fiscal-Software/Fiscal-Software/Controlles/SvidetelstvoRegistraciqCtrl.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fiscal_Software.Controlles
+{
+    class SvidetelstvoRegistraciqCtrl
+    {
+        public static void AddSvidetelstvoRegistraciq(SvidetelstvoRegistraciq svidetelstvo)
+        {
+
+            using (var ctx = new FiscalSoftware())
+            {
+                ctx.SvidetelstvoRegistraciq.Add(svidetelstvo);
+                ctx.SaveChanges();
+              
+            }
+
+        }
+        public static void UpdateSvidetelstvoRegistraciq(int id, SvidetelstvoRegistraciq svidetelstvo)
+        {
+            using (var ctx = new FiscalSoftware())
+            {
+                var original = ctx.SvidetelstvoRegistraciq.Where(b => b.id == id)
+                    .FirstOrDefault();
+
+                if (original != null)
+                {
+                    original.RegDate = svidetelstvo.RegDate;
+                    original.SvidetelstvoN = svidetelstvo.SvidetelstvoN;
+                    original.Technician = svidetelstvo.Technician;
+                    original.Contract = svidetelstvo.Contract;
+                    original.RegNoNap = svidetelstvo.RegNoNap;
+                    original.RegNoNapIzdaden = svidetelstvo.RegNoNapIzdaden;
+                    original.AutoNumbering = svidetelstvo.AutoNumbering;
+                    original.Notes = svidetelstvo.Notes;
+                    original.PrietObs = svidetelstvo.PrietObs;
+                    original.PrekratenoObs = svidetelstvo.PrekratenoObs;
+                    original.Reason = svidetelstvo.Reason;
+                    ctx.SaveChanges();
+                }
+            }
+        }
+        public static SvidetelstvoRegistraciq GetSvidetelstvoRegistraciqById(int id)
+        {
+            using (var ctx = new FiscalSoftware())
+            {
+                var svidetelstvo= ctx.SvidetelstvoRegistraciq.Where(b => b.id == id)
+                    .FirstOrDefault();
+                return svidetelstvo;
+            }
+        }
+        public static void DeleteSvidetelstvoRegistraciqById(int id)
+        {
+            using (var ctx = new FiscalSoftware())
+            {
+                var svidetelstvo = ctx.SvidetelstvoRegistraciq.Where(b => b.id == id)
+                    .FirstOrDefault();
+
+                if (svidetelstvo != null)
+                {
+                   
+
+                    ctx.SvidetelstvoRegistraciq.Remove(svidetelstvo);
+                    ctx.SaveChanges();
+
+
+                }
+            }
+
+
+        }
+    }
+}
