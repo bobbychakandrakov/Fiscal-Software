8bf9cbaf319c427b56ce97671ae6affb29ebeea0
dinchokostadinov <dincho1998@abv.bg>
2017-01-30 19:27:39 +0200

    all reports done!

--- Fiscal-Software/Fiscal-Software/Forms/PrintSvidetelstvo.cs
+++ Fiscal-Software/Fiscal-Software/Forms/PrintSvidetelstvo.cs
@@ -142,6 +142,9 @@ namespace Fiscal_Software.Forms
 
     */
 
+
+            /* RemontProtokol 
+             * 
             ReportParameter TodayDate = new ReportParameter("TodayDate", " ");
             ReportParameter CompnayName = new ReportParameter("CompnayName", " ");
             ReportParameter MOL = new ReportParameter("MOL", " ");
@@ -166,6 +169,63 @@ namespace Fiscal_Software.Forms
 
           }
           );
+          */
+
+            ReportParameter datetime = new ReportParameter("datetime", " ");
+            ReportParameter identNomer = new ReportParameter("identNomer", " ");
+            ReportParameter sobstvenostNa = new ReportParameter("sobstvenostNa", " ");
+            ReportParameter PredstavlqvanOt = new ReportParameter("PredstavlqvanOt", " ");
+            ReportParameter TurgovskiObekt = new ReportParameter("TurgovskiObekt", " ");
+            ReportParameter PlombiranOt = new ReportParameter("PlombiranOt", " ");
+            ReportParameter BimNomer = new ReportParameter("BimNomer", " ");
+            ReportParameter Dopk = new ReportParameter("Dopk", " ");
+            ReportParameter Fu = new ReportParameter("Fu", " ");
+            ReportParameter Fp = new ReportParameter("Fp", " ");
+            ReportParameter Fdrid = new ReportParameter("Fdrid", " ");
+            ReportParameter PrichinaDemontaj = new ReportParameter("PrichinaDemontaj", " ");
+            ReportParameter ot = new ReportParameter("ot", " ");
+            ReportParameter Do = new ReportParameter("do", " ");
+            ReportParameter suma = new ReportParameter("suma", " ");
+            ReportParameter DDsA = new ReportParameter("DDsA", " ");
+            ReportParameter DDsB = new ReportParameter("DDsB", " ");
+            ReportParameter DDsV = new ReportParameter("DDsV", " ");
+            ReportParameter DDsG = new ReportParameter("DDsG", " ");
+            ReportParameter serviznaFirma = new ReportParameter("serviznaFirma", " ");
+            ReportParameter Dopk2 = new ReportParameter("Dopk2", " ");
+            ReportParameter predstavitel = new ReportParameter("predstavitel", " ");
+            ReportParameter grad = new ReportParameter("grad", " ");
+            ReportParameter data = new ReportParameter("data", " ");
+            ReportParameter sobstvenik = new ReportParameter("sobstvenik", " ");
+
+            reportViewer1.LocalReport.SetParameters(new ReportParameter[]
+       {
+              datetime,
+              identNomer,
+              sobstvenostNa,
+              PredstavlqvanOt,
+              TurgovskiObekt,
+              PlombiranOt,
+              Dopk,
+              Fu,
+              Fp,
+              Fdrid,
+              PrichinaDemontaj,
+              ot,
+              Do,
+              suma,
+              DDsA,
+              DDsB,
+              DDsV,
+              DDsG,
+              serviznaFirma,
+              Dopk2,
+              predstavitel,
+              grad,
+              data,
+              sobstvenik
+
+       }
+       );
 
             reportViewer1.SetDisplayMode(DisplayMode.PrintLayout);
             this.reportViewer1.RefreshReport();
