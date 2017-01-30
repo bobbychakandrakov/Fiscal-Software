0c8497d0118a06143fdbaa775561397a54d789c1
dinchokostadinov <dincho1998@abv.bg>
2017-01-30 09:47:14 +0200

    empty params -2docs done!

--- Fiscal-Software/Fiscal-Software/Forms/PrintSvidetelstvo.cs
+++ Fiscal-Software/Fiscal-Software/Forms/PrintSvidetelstvo.cs
@@ -20,8 +20,116 @@ namespace Fiscal_Software.Forms
 
         private void PrintSvidetelstvo_Load(object sender, EventArgs e)
         {
-           
+
+            /*Uvedomlenie Report
+            ReportParameter Ot = new ReportParameter("ot", " ");
+            ReportParameter Na = new ReportParameter("na", " ");
+            ReportParameter Bulstat = new ReportParameter("Bulstat", " ");
+            ReportParameter NaselenoMqsto = new ReportParameter("naselenoMqsto", " ");
+            ReportParameter AdresUpravlenie = new ReportParameter("AdresUpravlenie", " ");
+            ReportParameter DanAdres = new ReportParameter("DanAdres", " ");
+            ReportParameter Telefon1 = new ReportParameter("telefon1", " ");
+            ReportParameter Email = new ReportParameter("email", " ");
+            ReportParameter NapReg = new ReportParameter("napReg", " ");
+            ReportParameter Ofis= new ReportParameter("ofis", " ");
+            ReportParameter Telefon2 = new ReportParameter("telefon2", " ");
+            ReportParameter ModelFiskalno = new ReportParameter("ModelFiskalno", " ");
+            ReportParameter indNomerFiskalno = new ReportParameter("indNomerFiskalno", " ");
+            ReportParameter IndNomerFiskalPamet = new ReportParameter("IndNomerFiskPamet", " ");
+            ReportParameter VavedenaServzinaFirma = new ReportParameter("VavedenaServiznafirma", " ");
+            ReportParameter ServizenTehnik = new ReportParameter("servizenTehnik", " ");
+            ReportParameter ServzienDogovor = new ReportParameter("servizenDogovor", " ");
+            ReportParameter ValidenOt = new ReportParameter("validenOt", " ");
+            ReportParameter ValidenDo = new ReportParameter("validenDo", " ");
+
+
+            reportViewer1.LocalReport.SetParameters(new ReportParameter[]
+            {
+                Ot,
+                Na,
+                Bulstat,
+                NaselenoMqsto,
+                AdresUpravlenie,
+                DanAdres,
+                Telefon1,
+                Email,
+                NapReg,
+                Ofis,
+                Telefon2,
+                ModelFiskalno,
+                indNomerFiskalno,
+                IndNomerFiskalPamet,
+                VavedenaServzinaFirma,
+                ServizenTehnik,
+                ServzienDogovor,
+                ValidenOt,
+                ValidenDo
+
+            }
+            );
+            */
+
+            ReportParameter RemontNomer = new ReportParameter("RemontNomer", " ");
+            ReportParameter todayDate = new ReportParameter("todayDate", " ");
+            ReportParameter vGrad = new ReportParameter("vGrad", " ");
+            ReportParameter serviznaOrganizaciq = new ReportParameter("serviznaOrganizaciq", " ");
+            ReportParameter predstavenaOt = new ReportParameter("predstavenaOt", " ");
+            ReportParameter organizaciqVazlojitel = new ReportParameter("organizaciqVazlojitel", " ");
+            ReportParameter identNomer = new ReportParameter("identNomer", " ");
+            ReportParameter addres1 = new ReportParameter("addres1", " ");
+            ReportParameter predstavenaOt2 = new ReportParameter(" predstavenaOt2", " ");
+            ReportParameter vidTehnika = new ReportParameter("vidTehnika", " ");
+            ReportParameter identNomer2 = new ReportParameter("identNomer2", " ");
+            ReportParameter NomerFp = new ReportParameter("NomerFp", " ");
+            ReportParameter taksa = new ReportParameter("taksa", " ");
+            ReportParameter validenOt = new ReportParameter("validenOt", " ");
+            ReportParameter validenDo = new ReportParameter("validenDo", " ");
+            ReportParameter BankovaSmetka = new ReportParameter("BankovaSmetka", " ");
+            ReportParameter data2 = new ReportParameter(" data2", " ");
+            ReportParameter EikBulstad = new ReportParameter("EikBulstad", " ");
+            ReportParameter sobstvenostNa = new ReportParameter("sobstvenostNa", " ");
+            ReportParameter address2 = new ReportParameter("address2", " ");
+            ReportParameter predstavlqvanaOt = new ReportParameter("predstavlqvanaOt", " ");
+            ReportParameter TargovskiObekt= new ReportParameter("TargovskiObek", " ");
+            ReportParameter Model = new ReportParameter(" Model", " ");
+            ReportParameter BimNomer = new ReportParameter("BimNomer", " ");
+            ReportParameter indNomerNafiskalnotoUstroistvo = new ReportParameter(" indNomerNafiskalnotoUstroistvo", " ");
+            ReportParameter FiskalnaPamet = new ReportParameter("FiskalnaPamet", " ");
+            ReportParameter ValidenOt2 = new ReportParameter("ValidenOt2", " ");
+            ReportParameter Eik = new ReportParameter("Eik", " ");
+            ReportParameter Adress3 = new ReportParameter("Adress3", " ");
+            ReportParameter ServzienTehnik = new ReportParameter("ServzienTehnik", " ");
+            ReportParameter Data = new ReportParameter("Data", " ");
+
+            reportViewer1.LocalReport.SetParameters(new ReportParameter[]
+           {
+                RemontNomer,
+                todayDate,
+                vGrad,
+                serviznaOrganizaciq,
+                predstavenaOt,
+                organizaciqVazlojitel,
+                identNomer,
+                addres1,
+               predstavenaOt2,
+               vidTehnika,
+                identNomer2,
+               NomerFp,
+                taksa,
+               validenOt,
+                validenDo,
+                BankovaSmetka,
+               data2,
+                EikBulstad,
+                sobstvenostNa
+
+           }
+           );
+
+
+            reportViewer1.SetDisplayMode(DisplayMode.PrintLayout);
             this.reportViewer1.RefreshReport();
+           
         }
     }
 }
